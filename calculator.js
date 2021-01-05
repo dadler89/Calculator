e1c22add703461645dc9b06f0a0fc97a6543ca85
a.adler89@gmail.com <69403909+dadler89@users.noreply.github.com>
2021-01-05 13:51:14 -0600

    first commit

---
+++ calculator.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", (req, res) => {
+
+var num1 = Number(req.body.num1)
+var num2 = Number(req.body.num2)
+
+var result = num1 + num2;
+
+  res.send("The result of your calculation is " + result)
+})
+
+app.listen(3000, () => {
+  console.log("Andre Listening on port 3000")
+})
